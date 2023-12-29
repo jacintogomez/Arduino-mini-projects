22d0a7c86572d179e2aed1fa1ba92ded163f8392
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-12-29 00:39:27 -0500

    adding test files

--- morse.cpp
+++ morse.cpp
@@ -2,14 +2,34 @@
 #include <vector>
 using namespace std;
 
-vector<char> tomorse(string ph){
+vector<char> bank(char d){
+    vector<char> r;
+    if(d=='a'){r={'.','-'};}
+    return r;
+}
 
+vector<vector<char>> tomorse(string ph){
+    vector<vector<char>> collect;
+    for(char d:ph){
+        collect.push_back(bank(d));
+    }
+    return collect;
+}
+
+void blink(vector<vector<char>> in){
+    for(vector<char> i:in){
+        for(char j:i){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
     string phrase;
     cout<<"Enter sentence: "<<endl;
     cin>>phrase;
-    vector<char> morse=tomorse(phrase);
-
+    vector<vector<char>> morse=tomorse(phrase);
+    blink(morse);
     return 0;
 }
