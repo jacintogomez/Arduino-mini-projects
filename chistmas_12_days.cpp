f1f8016542391004fb4b7da8e42382adfa0f8c9c
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2023-12-29 19:11:40 -0500

    new development

--- chistmas_12_days.cpp
+++ chistmas_12_days.cpp
@@ -1,47 +1,22 @@
 //Use Arduino to control blinking Christmas lights
 
-#define d1 2
-#define d2 3
-#define d3 4
-#define d4 5
-#define d5 6
-#define d6 7
-#define d7 8
-#define d8 9
-#define d9 10
-#define d10 11
-#define d11 12
-#define d12 13
+#define numpins 12
+int pins[numpins]={2,3,4,5,6,7,8,9,10,11,12,13};
 
 void setup(){
-    pinMode(d1,OUTPUT);
-    pinMode(d2,OUTPUT);
-    pinMode(d3,OUTPUT);
-    pinMode(d4,OUTPUT);
-    pinMode(d5,OUTPUT);
-    pinMode(d6,OUTPUT);
-    pinMode(d7,OUTPUT);
-    pinMode(d8,OUTPUT);
-    pinMode(d9,OUTPUT);
-    pinMode(d10,OUTPUT);
-    pinMode(d11,OUTPUT);
-    pinMode(d12,OUTPUT);
+    for(int i=0;i<numpins;i++){
+        pinMode(pins[i],OUTPUT);
+    }
     delay(1000);
 }
 
 int whatdayisit(){
-    if(digitalRead(d1)==HIGH){return d1;}
-    if(digitalRead(d2)==HIGH){return d2;}
-    if(digitalRead(d3)==HIGH){return d3;}
-    if(digitalRead(d4)==HIGH){return d4;}
-    if(digitalRead(d5)==HIGH){return d5;}
-    if(digitalRead(d6)==HIGH){return d6;}
-    if(digitalRead(d7)==HIGH){return d7;}
-    if(digitalRead(d8)==HIGH){return d8;}
-    if(digitalRead(d9)==HIGH){return d9;}
-    if(digitalRead(d10)==HIGH){return d10;}
-    if(digitalRead(d11)==HIGH){return d11;}
-    if(digitalRead(d12)==HIGH){return d12;}
+    for(int j=0;j<numpins;j++){
+        if(digitalRead(pins[j])==HIGH){
+            return pins[j];
+        }
+    }
+    return -1;
 }
 
 void turnoffandon(int n){
